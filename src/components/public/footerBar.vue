c65e94ec3855a69d7a230be0d98a06ef73f8b74f
陆有名 <443630395@qq.com>
2020-05-31 17:59:30 +0800

    footer组件涉及DefaultContainerStyle抽出

--- src/components/public/footerBar.vue
+++ src/components/public/footerBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-footer>
+  <el-footer class="footer-container">
       footer-bar
   </el-footer>
 </template>
@@ -10,6 +10,13 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.footer-container{
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 26PX;
+  color: white;
+}
 </style>
