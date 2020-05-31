01904719beef98b253f4bc31545d1ed24cf6cca0
陆有名 <443630395@qq.com>
2020-05-31 17:49:43 +0800

    新增应用领域及子菜单页面

---
+++ src/views/Apparea/all.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="Apparea-container DefaultContainerStyle">所有</div>
+</template>
+
+<script>
+export default {
+  name: 'appareaAll'
+}
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/views/Apparea/introduction.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="Apparea-intro-container DefaultContainerStyle">简介</div>
+</template>
+
+<script>
+export default {
+  name: 'appareaIntroduction'
+}
+</script>
+
+<style scoped lang="scss">
+</style>
