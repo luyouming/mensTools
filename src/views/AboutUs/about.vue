5a4594610ddbf0137b1682aab17a10e8abdd359e
陆有名 <443630395@qq.com>
2020-05-31 17:49:19 +0800

    新增关于我们及子菜单页面

---
+++ src/views/AboutUs/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="AboutUs-container DefaultContainerStyle">关于我们</div>
+</template>
+
+<script>
+export default {
+  name: 'aboutUsAbout'
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/AboutUs/index.vue
+++ src/views/AboutUs/index.vue
@@ -1,33 +1,39 @@
 <template>
-  <div class="AboutUs-Container">
-    <div class="tip">{{msg}}</div>
-  </div>
+  <el-container direction="vertical">
+    <hangingBar />      
+    <top-bar :chosenTabIndex="1" @changePage="gotoPage"/>
+    <router-view class="main-page" />
+    <footer-bar />
+  </el-container>
 </template>
 
 <script>
+import hangingBar from '@/components/public/hangingBar'
+import topBar from '@/components/public/topBar'
+import footerBar from '@/components/public/footerBar'
 export default {
   name: 'AboutUs',
+  components: {
+    hangingBar,
+    topBar,
+    footerBar
+  },
   data () {
     return {
-      msg: '你好啊，AboutUs！'
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    gotoPage(path){
+      // console.log(path)
+      this.$router.push({path})
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.AboutUs-Container {
-  .tip{
-     // 不写会直接编译成对应的rem
-    width: 1920px;
-    height: 300px; 
-    line-height: 300px;
-    // 写了/*px*/ 会根据dpr不同生成对应的大小 ----sass 不支持
-    font-size: 36px; /*px*/
-    // 写了/*no*/那就不会转换   ----sass 不支持，要想不自动转换 需要大写
-    border:1PX solid red; /*no*/
-    color:green;
-  }
+.main-page{
+  min-height: 60vh;
 }
 </style>
