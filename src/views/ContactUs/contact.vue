75011cd48d42df9fe1ef3cc6fa4d885984e2a9e2
陆有名 <443630395@qq.com>
2020-05-31 17:55:22 +0800

    联系我们页面路由新建，相关子路由配置，视图完成

---
+++ src/views/ContactUs/contact.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="ContactUs-container DefaultContainerStyle">联系我们</div>
+</template>
+
+<script>
+export default {
+  name: 'contactUsContact'
+}
+</script>
+
+<style scoped lang="scss">
+</style>
