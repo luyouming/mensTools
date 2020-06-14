d1f48b6b7828bd087be1d00f3d964ad318032baf
陆有名 <443630395@qq.com>
2020-06-14 17:03:32 +0800

    友情链接add,联系我们样式调整美化，公共信息commoninfo.js存储位置提交，标题栏大小美化，关于我们add，应用领域add，首页完善

--- src/views/Apparea/all.vue
+++ src/views/Apparea/all.vue
@@ -1,16 +1,58 @@
 <template>
-  <div class="Apparea-container DefaultContainerStyle">
+  <div class="Apparea-all-container DefaultContainerStyle">
     <title-bar text="所有" />
+    <!-- 产品列表展示 -->
+    <div class="content">
+      <div class="productBox">
+        <el-row :gutter="30">
+          <el-col
+            v-for="(item, index) in productList"
+            :xs="24"
+            :sm="12"
+            :md="6"
+            :lg="6"
+            :xl="6"
+            :key="index + item"
+          >
+            <productDiscribe :productInfo="item" :isEven="(index + 1) % 2 == 0"></productDiscribe>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import titleBar from "$components/public/titleBar";
+import productDiscribe from "@/components/public/productDiscribe";
+import commonInfo from '@/assets/CommonJs/commonInfo'
 export default {
   name: 'appareaAll',
-  components: { titleBar }
+  data () {
+    return {
+      productList: commonInfo.productList
+    }
+  },
+  components: { titleBar, productDiscribe }
 }
 </script>
 
 <style scoped lang="scss">
+.Apparea-all-container{
+ display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+ width: 100%;
+ .content {
+    width: 100%;
+    .productBox {
+      width: 100%;
+      padding: 0 130px;
+      box-sizing: border-box;
+      margin: 120px 0 40px 0;
+    }
+ }
+}
+
 </style>
